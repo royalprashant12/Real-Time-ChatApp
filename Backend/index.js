cb5c1a33128fb66fb954758e3210daed44f5b8b4
prashant kumar <vtu20533@veltech.edu.in>
2025-07-04 12:34:18 +0530

    project completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,60 +3,44 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import http from "http";
-import { Server as SocketIOServer } from "socket.io";
-
-const app = express();
-const server = http.createServer(app);
-const io = new SocketIOServer(server, {
-  cors: {
-    origin: "*", // Adjust as needed for security
-    methods: ["GET", "POST"]
-  }
-});
-dotenv.config();
-// to allow cross-origin requests
-// to allow cross-origin requests
-
-const port = process.env.PORT || 4000;
-const dbUrl = process.env.MOONGODB_URI;
-// MongoDB connection URL from environment variables
-app.use(cookieParser()); // to parse cookies
-// Middleware to parse cookies
-app.use(express.json()); // to parse JSON bodies
-// Middleware to parse JSON bodies
 
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
+import path from "path";
+
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 4001;
+const URI = process.env.MONGODB_URI;
+
 try {
-  mongoose.connect(dbUrl);
+  mongoose.connect(URI);
   console.log("Connected to MongoDB");
 } catch (error) {
-  console.error("Error connecting to MongoDB:", error);
+  console.log(error);
 }
+
+//routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
-// with the help of middleware we send userroute to the user
 
-const userSocketMap = {};
-
-io.on("connection", (socket) => {
-  const userId = socket.handshake.query.userId;
-  if (userId) {
-    userSocketMap[userId] = socket.id;
-  }
-  console.log("A user connected: " + socket.id);
-
-  socket.on("disconnect", () => {
-    if (userId) {
-      delete userSocketMap[userId];
-    }
-    console.log("A user disconnected: " + socket.id);
+/*------ code for deployement ------*/
+if(process.env.NODE_ENV === "production"){
+  const dirPath = path.resolve();
+  app.use(express.static("./Frontend/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
   });
-});
+}
 
-export { io, userSocketMap };
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// server
+server.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
 });
