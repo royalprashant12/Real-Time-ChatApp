d4237d90f5822c6257ca00fec2d2288bf7024b1e
prashant kumar <vtu20533@veltech.edu.in>
2025-07-04 12:59:41 +0530

    project completed

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("https://real-time-chatapp-iclr.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
